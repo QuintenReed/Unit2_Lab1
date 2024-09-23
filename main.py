cfd7692c9ffaf2f1af49beb0302187878a857511
Quinten Reed <qreed26@cpatech.org>
2024-09-23 14:23:29 +0000

    my notes

---
+++ main.py
@@ -0,0 +1,16 @@
+# Quinten Reed
+# U2L1
+# Multiplication Table
+
+# ONE LINE - NO MORE, NO LESS
+
+table = [x*y for x in range(1, 11) for y in range(1, 11)]
+
+########### NO TOUCHY ###########
+for i, num in enumerate(table):
+    if i % 10 == 0:
+        print()
+    
+    print(num, end="\t")
+print()
+#################################
